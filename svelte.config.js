71ae93f9a2e725c1c585a551fc57abe140559b40
Carina.Akaia.io <cvo.akaia@gmail.com>
2024-12-23 16:01:09 +0000

    Use Node polyfills

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
 import { preprocessMeltUI, sequence } from "@melt-ui/pp";
-import adapterAuto from "@sveltejs/adapter-auto";
+// import adapterAuto from "@sveltejs/adapter-auto";
+import adapterStatic from "@sveltejs/adapter-static";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config}*/
@@ -9,7 +10,7 @@ const config = {
 	preprocess: sequence([vitePreprocess(), preprocessMeltUI()]),
 
 	kit: {
-		adapter: adapterAuto(),
+		adapter: adapterStatic(),
 
 		env: {
 			publicPrefix: "PUBLIC_",
--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
 import extractorSvelte from "@unocss/extractor-svelte";
 import UnoCSS from "unocss/vite";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 export default defineConfig({
 	plugins: [
@@ -9,5 +10,9 @@ export default defineConfig({
 			extractors: [extractorSvelte()],
 		}),
 		sveltekit(),
+
+		nodePolyfills({
+			globals: { Buffer: true },
+		}),
 	],
 });
