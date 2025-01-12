546b2b5b47b6e0a4dc1c251c873eb4de81e4418d
Carina.Akaia.io <cvo.akaia@gmail.com>
2025-01-12 07:20:47 +0000

    wip

--- src/lib/common/api/near/client.ts
+++ src/lib/common/api/near/client.ts
@@ -39,9 +39,9 @@ export const initWalletSelector = async () =>
 			// }),
 
 			setupMyNearWallet(),
+			setupMeteorWallet(),
 			setupSender(),
 			setupHereWallet(),
-			setupMeteorWallet(),
 			setupLedger(),
 			setupNearMobileWallet(),
 			setupNightly(),
